4fafc3e52ae9c0aea04212d9d9a27baa21ae845e
linj-disanbo <linj@disanbo.com>
2022-04-13 15:42:21 +0800

    本来想直接把代码分开, 模块化的, 要改动的太多了 换个分支实现和验证功能

--- plugin/dapp/zkspot/plugin.go
+++ plugin/dapp/zkspot/plugin.go
@@ -8,6 +8,8 @@ import (
 	"github.com/33cn/plugin/plugin/dapp/zkspot/types"
 )
 
+// 本来想直接把代码分开, 模块化的, 要改动的太多了
+// 先换个分支实现和验证功能
 func init() {
 	pluginmgr.Register(&pluginmgr.PluginBase{
 		Name:     types.Zksync,
