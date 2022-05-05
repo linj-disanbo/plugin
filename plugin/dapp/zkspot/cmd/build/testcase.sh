63c7e9af53861cae30b0473a6488d0ce6b06f74a
linj-disanbo <linj@disanbo.com>
2022-05-05 17:48:57 +0800

    测试脚本更新

--- plugin/dapp/zkspot/cmd/build/testcase.sh
+++ plugin/dapp/zkspot/cmd/build/testcase.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
 #1ks returner chain31
-ZKSYNC_CLI31="docker exec ${NODE1} /root/chain33-cli "
+zkspot_CLI31="docker exec ${NODE1} /root/chain33-cli "
 #1jr  authorize chain32
-ZKSYNC_CLI32="docker exec ${NODE2} /root/chain33-cli "
+zkspot_CLI32="docker exec ${NODE2} /root/chain33-cli "
 #1nl receiver  chain30
-ZKSYNC_CLI30="docker exec ${NODE4} /root/chain33-cli "
+zkspot_CLI30="docker exec ${NODE4} /root/chain33-cli "
 
 # shellcheck source=/dev/null
 #source test-rpc.sh
 
-function zksync_set_wallet() {
-    echo "=========== # zksync set wallet ============="
+function zkspot_set_wallet() {
+    echo "=========== # zkspot set wallet ============="
     #1KSBd17H7ZK8iT37aJztFB22XGwsPTdwE4
-    zksync_import_wallet "${ZKSYNC_CLI31}" "0x6da92a632ab7deb67d38c0f6560bcfed28167998f6496db64c258d5e8393a81b" "account1"
+    zkspot_import_wallet "${zkspot_CLI31}" "0x6da92a632ab7deb67d38c0f6560bcfed28167998f6496db64c258d5e8393a81b" "account1"
     #1JRNjdEqp4LJ5fqycUBm9ayCKSeeskgMKR
-    zksync_import_wallet "${ZKSYNC_CLI32}" "0x19c069234f9d3e61135fefbeb7791b149cdf6af536f26bebb310d4cd22c3fee4" "account2"
+    zkspot_import_wallet "${zkspot_CLI32}" "0x19c069234f9d3e61135fefbeb7791b149cdf6af536f26bebb310d4cd22c3fee4" "account2"
     #1NLHPEcbTWWxxU3dGUZBhayjrCHD3psX7k
-    zksync_import_wallet "${ZKSYNC_CLI30}" "0x7a80a1f75d7360c6123c32a78ecf978c1ac55636f87892df38d8b85a9aeff115" "account3"
+    zkspot_import_wallet "${zkspot_CLI30}" "0x7a80a1f75d7360c6123c32a78ecf978c1ac55636f87892df38d8b85a9aeff115" "account3"
 }
 
-function zksync_import_wallet() {
+function zkspot_import_wallet() {
     local lable=$3
     echo "=========== # save seed to wallet ============="
     result=$(${1} seed save -p 1314fuzamei -s "tortoise main civil member grace happy century convince father cage beach hip maid merry rib" | jq ".isok")
@@ -46,8 +46,8 @@ function zksync_import_wallet() {
     ${1} wallet status
 }
 
-function zksync_init() {
-    echo "=========== # zksync chain init ============="
+function zkspot_init() {
+    echo "=========== # zkspot chain init ============="
 
     #account1
     ${CLI} send coins transfer -a 200 -n transfer -t 1KSBd17H7ZK8iT37aJztFB22XGwsPTdwE4 -k 4257D8692EF7FE13C68B65D6A52F03933DB2FA5CE8FAF210B5B8B80C721CED01
@@ -58,25 +58,25 @@ function zksync_init() {
 
 }
 
-function zksync_deposit() {
-  echo "=========== # zksync deposit test ============="
+function zkspot_deposit() {
+  echo "=========== # zkspot deposit test ============="
     #1KSBd17H7ZK8iT37aJztFB22XGwsPTdwE4 deposit amount 1000000000000
-    chain33Addr=$(${CLI} zksync getChain33Addr -k 6da92a632ab7deb67d38c0f6560bcfed28167998f6496db64c258d5e8393a81b)
-    rawData=$(${CLI} zksync deposit -t 1 -a 1000000000000000000000 -e abcd68033A72978C1084E2d44D1Fa06DdC4A2d57 -c "$chain33Addr")
+    chain33Addr=$(${CLI} zkspot getChain33Addr -k 6da92a632ab7deb67d38c0f6560bcfed28167998f6496db64c258d5e8393a81b)
+    rawData=$(${CLI} zkspot deposit -t 1 -a 1000000000000000000000 -e abcd68033A72978C1084E2d44D1Fa06DdC4A2d57 -c "$chain33Addr")
     echo "${rawData}"
 
     signData=$(${CLI} wallet sign -d "$rawData" -k 4257D8692EF7FE13C68B65D6A52F03933DB2FA5CE8FAF210B5B8B80C721CED01)
     echo "${signData}"
     hash=$(${CLI} wallet send -d "$signData")
     echo "${hash}"
-    query_tx "${CLI}" "${hash}"
+    query_tx "${CLI}" "${hash}" # bug ErrExecPanic
     query_account "${CLI}" 1
 }
 
-function zksync_setPubKey() {
-    echo "=========== # zksync setPubKey test ============="
+function zkspot_setPubKey() {
+    echo "=========== # zkspot setPubKey test ============="
     #1KSBd17H7ZK8iT37aJztFB22XGwsPTdwE4 setPubKey
-    rawData=$(${CLI} zksync setPubKey -a 1)
+    rawData=$(${CLI} zkspot setPubKey -a 1)
     echo "${rawData}"
 
     signData=$(${CLI} wallet sign -d "$rawData" -k 0x6da92a632ab7deb67d38c0f6560bcfed28167998f6496db64c258d5e8393a81b)
@@ -88,10 +88,10 @@ function zksync_setPubKey() {
 
 }
 
-function zksync_withdraw() {
-    echo "=========== # zksync withdraw test ============="
+function zkspot_withdraw() {
+    echo "=========== # zkspot withdraw test ============="
     #1KSBd17H7ZK8iT37aJztFB22XGwsPTdwE4 withdraw amount 100000000
-    rawData=$(${CLI} zksync withdraw -t 1 -a 100000000 --accountId 1)
+    rawData=$(${CLI} zkspot withdraw -t 1 -a 100000000 --accountId 1)
     echo "${rawData}"
 
     signData=$(${CLI} wallet sign -d "$rawData" -k 0x6da92a632ab7deb67d38c0f6560bcfed28167998f6496db64c258d5e8393a81b)
@@ -102,10 +102,10 @@ function zksync_withdraw() {
     query_account "${CLI}" 1
 }
 
-function zksync_treeToContract() {
-    echo "=========== # zksync treeToContract test ============="
+function zkspot_treeToContract() {
+    echo "=========== # zkspot treeToContract test ============="
     #1KSBd17H7ZK8iT37aJztFB22XGwsPTdwE4 treeToContract amount 1000000000
-    rawData=$(${CLI} zksync treeToContract -t 1 -a 10000000000000000000 --accountId 1)
+    rawData=$(${CLI} zkspot treeToContract -t 1 -a 10000000000000000000 --accountId 1)
     echo "${rawData}"
 
     signData=$(${CLI} wallet sign -d "$rawData" -k 0x6da92a632ab7deb67d38c0f6560bcfed28167998f6496db64c258d5e8393a81b)
@@ -116,11 +116,11 @@ function zksync_treeToContract() {
     query_account "${CLI}" 1
 }
 
-function zksync_contractToTree() {
-    echo "=========== # zksync contractToTree test ============="
+function zkspot_contractToTree() {
+    echo "=========== # zkspot contractToTree test ============="
     #1KSBd17H7ZK8iT37aJztFB22XGwsPTdwE4 contractToTree to self amount 100000000
-    chain33Addr=$(${CLI} zksync getChain33Addr -k 6da92a632ab7deb67d38c0f6560bcfed28167998f6496db64c258d5e8393a81b)
-    rawData=$(${CLI} zksync contractToTree -t 1 -a 1000000000000000000 --accountId 1)
+    chain33Addr=$(${CLI} zkspot getChain33Addr -k 6da92a632ab7deb67d38c0f6560bcfed28167998f6496db64c258d5e8393a81b)
+    rawData=$(${CLI} zkspot contractToTree -t 1 -a 1000000000000000000 --accountId 1)
     echo "${rawData}"
 
     signData=$(${CLI} wallet sign -d "$rawData" -k 0x6da92a632ab7deb67d38c0f6560bcfed28167998f6496db64c258d5e8393a81b)
@@ -131,8 +131,8 @@ function zksync_contractToTree() {
     query_account "${CLI}" 1
 
     #1JRNjdEqp4LJ5fqycUBm9ayCKSeeskgMKR deposit amount 1000000000
-    chain33Addr=$(${CLI} zksync getChain33Addr -k 19c069234f9d3e61135fefbeb7791b149cdf6af536f26bebb310d4cd22c3fee4)
-    rawData=$(${CLI} zksync deposit -t 1 -a 1000000000 -e abcd68033A72978C1084E2d44D1Fa06DdC4A2d57 -c "$chain33Addr")
+    chain33Addr=$(${CLI} zkspot getChain33Addr -k 19c069234f9d3e61135fefbeb7791b149cdf6af536f26bebb310d4cd22c3fee4)
+    rawData=$(${CLI} zkspot deposit -t 1 -a 1000000000 -e abcd68033A72978C1084E2d44D1Fa06DdC4A2d57 -c "$chain33Addr")
     echo "${rawData}"
 
     signData=$(${CLI} wallet sign -d "$rawData" -k 4257D8692EF7FE13C68B65D6A52F03933DB2FA5CE8FAF210B5B8B80C721CED01)
@@ -143,10 +143,10 @@ function zksync_contractToTree() {
     query_account "${CLI}" 2
 }
 
-function zksync_transfer() {
-    echo "=========== # zksync transfer test ============="
+function zkspot_transfer() {
+    echo "=========== # zkspot transfer test ============="
     #1KSBd17H7ZK8iT37aJztFB22XGwsPTdwE4 transfer to 1JRNjdEqp4LJ5fqycUBm9ayCKSeeskgMKR amount 100000000
-    rawData=$(${CLI} zksync transfer -t 1 -a 100000000 --accountId 1 --toAccountId 2)
+    rawData=$(${CLI} zkspot transfer -t 1 -a 100000000 --accountId 1 --toAccountId 2)
     echo "${rawData}"
 
     signData=$(${CLI} wallet sign -d "$rawData" -k 0x6da92a632ab7deb67d38c0f6560bcfed28167998f6496db64c258d5e8393a81b)
@@ -158,11 +158,11 @@ function zksync_transfer() {
     query_account "${CLI}" 2
 }
 
-function zksync_transferToNew() {
-    echo "=========== # zksync transferToNew test ============="
+function zkspot_transferToNew() {
+    echo "=========== # zkspot transferToNew test ============="
     #1KSBd17H7ZK8iT37aJztFB22XGwsPTdwE4 transferToNew to 1NLHPEcbTWWxxU3dGUZBhayjrCHD3psX7k amount 100000000
-    chain33Addr=$(${CLI} zksync getChain33Addr -k 7a80a1f75d7360c6123c32a78ecf978c1ac55636f87892df38d8b85a9aeff115)
-    rawData=$(${CLI} zksync transferToNew -t 1 -a 100000000 --accountId 1 -e 12a0E25E62C1dBD32E505446062B26AECB65F028 -c "$chain33Addr")
+    chain33Addr=$(${CLI} zkspot getChain33Addr -k 7a80a1f75d7360c6123c32a78ecf978c1ac55636f87892df38d8b85a9aeff115)
+    rawData=$(${CLI} zkspot transferToNew -t 1 -a 100000000 --accountId 1 -e 12a0E25E62C1dBD32E505446062B26AECB65F028 -c "$chain33Addr")
     echo "${rawData}"
 
     signData=$(${CLI} wallet sign -d "$rawData" -k 0x6da92a632ab7deb67d38c0f6560bcfed28167998f6496db64c258d5e8393a81b)
@@ -173,10 +173,10 @@ function zksync_transferToNew() {
     query_account "${CLI}" 3
 }
 
-function zksync_forceExit() {
-    echo "=========== # zksync forceExit test ============="
+function zkspot_forceExit() {
+    echo "=========== # zkspot forceExit test ============="
     #1JRNjdEqp4LJ5fqycUBm9ayCKSeeskgMKR setPubKey
-    rawData=$(${CLI} zksync setPubKey -a 2)
+    rawData=$(${CLI} zkspot setPubKey -a 2)
     echo "${rawData}"
 
     signData=$(${CLI} wallet sign -d "$rawData" -k 0x19c069234f9d3e61135fefbeb7791b149cdf6af536f26bebb310d4cd22c3fee4)
@@ -187,7 +187,7 @@ function zksync_forceExit() {
     query_account "${CLI}" 2
 
     #1KSBd17H7ZK8iT37aJztFB22XGwsPTdwE4 help 1JRNjdEqp4LJ5fqycUBm9ayCKSeeskgMKR forceExit
-    rawData=$(${CLI} zksync forceExit -t 1 --accountId 2)
+    rawData=$(${CLI} zkspot forceExit -t 1 --accountId 2)
     echo "${rawData}"
 
     signData=$(${CLI} wallet sign -d "$rawData" -k 0x6da92a632ab7deb67d38c0f6560bcfed28167998f6496db64c258d5e8393a81b)
@@ -198,10 +198,10 @@ function zksync_forceExit() {
     query_account "${CLI}" 2
 }
 
-function zksync_fullExit() {
-    echo "=========== # zksync fullExit test ============="
+function zkspot_fullExit() {
+    echo "=========== # zkspot fullExit test ============="
     #1NLHPEcbTWWxxU3dGUZBhayjrCHD3psX7k setPubKey
-    rawData=$(${CLI} zksync setPubKey -a 3)
+    rawData=$(${CLI} zkspot setPubKey -a 3)
     echo "${rawData}"
 
     signData=$(${CLI} wallet sign -d "$rawData" -k 0x7a80a1f75d7360c6123c32a78ecf978c1ac55636f87892df38d8b85a9aeff115)
@@ -212,7 +212,7 @@ function zksync_fullExit() {
     query_account "${CLI}" 3
 
     #1NLHPEcbTWWxxU3dGUZBhayjrCHD3psX7k fullExit
-    rawData=$(${CLI} zksync fullExit -t 1 --accountId 3)
+    rawData=$(${CLI} zkspot fullExit -t 1 --accountId 3)
     echo "${rawData}"
 
     signData=$(${CLI} wallet sign -d "$rawData" -k 4257D8692EF7FE13C68B65D6A52F03933DB2FA5CE8FAF210B5B8B80C721CED01)
@@ -223,10 +223,10 @@ function zksync_fullExit() {
     query_account "${CLI}" 3
 }
 
-function zksync_setVerifyKey() {
-    echo "=========== # zksync setVerifyKey test ============="
+function zkspot_setVerifyKey() {
+    echo "=========== # zkspot setVerifyKey test ============="
     #set verify key
-    rawData=$(${CLI} zksync vkey -v acd216c9824f2388a5bb59427d91795bf2002b2b18ae28e7b65ea2fe2e736983c843cddb4e15ffbd0e7d1b6a1832d84502b792a6ecdf852f86e0fb9c95b8ed0adfc8d3ef755b095cfb0d82f66ce6cbc310aa5e6874052daa7821d0a5019454a22d925d976c93bcf872e46c18b6706368ac07b85f56565144f7edc456fed8e8f8adaba984afe9d46fe11f454a97f8725614fe2b33e2fd4acda5deab9d9d7b450527a546e83fd53d6db4a86a70a2126b245dc6cc1f23adbe60efa8613074c71face7cc6380e129b5426ba93adddc2e3792daf108e18adb3d23e5eba4ad338963b1d54c4fd75976b10a111ca81ea48ad37deb4577bb3d78370d5ab444edde28e3052b785b3314df302c589ffc47745b4097f48bc9afd49aed407230adac614d6ff200000003d5e5c30a45d7ca6c761e3e97178b9b0fc9a0802d6e6bf0b293b318b5922beab3ae95b9955ad90c875e983e9ef167cdac3de470a618e7632373afd3f9d4374dbbcf82d3a5074a9c4ff4664c6c9b292de7f1e96a1054addb0c0514c10dcf1d5403)
+    rawData=$(${CLI} zkspot vkey -v acd216c9824f2388a5bb59427d91795bf2002b2b18ae28e7b65ea2fe2e736983c843cddb4e15ffbd0e7d1b6a1832d84502b792a6ecdf852f86e0fb9c95b8ed0adfc8d3ef755b095cfb0d82f66ce6cbc310aa5e6874052daa7821d0a5019454a22d925d976c93bcf872e46c18b6706368ac07b85f56565144f7edc456fed8e8f8adaba984afe9d46fe11f454a97f8725614fe2b33e2fd4acda5deab9d9d7b450527a546e83fd53d6db4a86a70a2126b245dc6cc1f23adbe60efa8613074c71face7cc6380e129b5426ba93adddc2e3792daf108e18adb3d23e5eba4ad338963b1d54c4fd75976b10a111ca81ea48ad37deb4577bb3d78370d5ab444edde28e3052b785b3314df302c589ffc47745b4097f48bc9afd49aed407230adac614d6ff200000003d5e5c30a45d7ca6c761e3e97178b9b0fc9a0802d6e6bf0b293b318b5922beab3ae95b9955ad90c875e983e9ef167cdac3de470a618e7632373afd3f9d4374dbbcf82d3a5074a9c4ff4664c6c9b292de7f1e96a1054addb0c0514c10dcf1d5403)
     echo "${rawData}"
 
     signData=$(${CLI} wallet sign -d "$rawData" -k 4257D8692EF7FE13C68B65D6A52F03933DB2FA5CE8FAF210B5B8B80C721CED01)
@@ -240,7 +240,7 @@ function query_account() {
     block_wait "${1}" 1
 
     local times=200
-    ret=$(${1} zksync account -a "${2}")
+    ret=$(${1} zkspot account -a "${2}")
     echo "query account accountId=${2}, return ${ret} "
 
 }
@@ -272,12 +272,12 @@ function create_tx() {
     local accountId=4
     while true; do
          #loop deposit amount 1000000000000
-         echo "=========== # zksync setVerifyKey test ============="
+         echo "=========== # zkspot setVerifyKey test ============="
          privateKey=$(${CLI} account rand -l 1 | jq -r ".privateKey")
          echo "${privateKey}"
-         chain33Addr=$(${CLI} zksync getChain33Addr -k "$privateKey")
+         chain33Addr=$(${CLI} zkspot getChain33Addr -k "$privateKey")
 
-         rawData=$(${CLI} zksync deposit -t 1 -a 1000000000000 -e abcd68033A72978C1084E2d44D1Fa06DdC4A2d57 -c "$chain33Addr")
+         rawData=$(${CLI} zkspot deposit -t 1 -a 1000000000000 -e abcd68033A72978C1084E2d44D1Fa06DdC4A2d57 -c "$chain33Addr")
          echo "${rawData}"
 
          signData=$(${CLI} wallet sign -d "$rawData" -k 4257D8692EF7FE13C68B65D6A52F03933DB2FA5CE8FAF210B5B8B80C721CED01)
@@ -290,29 +290,28 @@ function create_tx() {
     done
 }
 
-function zksync_test() {
-    echo "=========== # zksync chain test ============="
-    zksync_deposit
-    zksync_setPubKey
-    zksync_withdraw
-    zksync_treeToContract
-    zksync_contractToTree
-    zksync_transfer
-    zksync_transferToNew
-    zksync_forceExit
-    zksync_fullExit
-    zksync_setVerifyKey
+function zkspot_test() {
+    echo "=========== # zkspot chain test ============="
+    zkspot_deposit
+    zkspot_setPubKey
+    zkspot_withdraw
+    zkspot_treeToContract
+    zkspot_contractToTree
+    zkspot_transfer
+    zkspot_transferToNew
+    zkspot_forceExit
+    zkspot_fullExit
+    zkspot_setVerifyKey
     create_tx
 }
 
-function zksync() {
+function zkspot() {
     if [ "${2}" == "init" ]; then
-        echo "zksync init"
+        echo "zkspot init"
     elif [ "${2}" == "config" ]; then
-        zksync_set_wallet
-        zksync_init
+        zkspot_set_wallet
+        zkspot_init
     elif [ "${2}" == "test" ]; then
-        zksync_test "${1}"
+        zkspot_test "${1}"
     fi
-
 }
