b5ffc955d92e4b7bed294a0791badc8b9f484d7c
linj-disanbo <linj@disanbo.com>
2022-06-06 11:12:28 +0800

    update entrust

--- plugin/dapp/zkspot/executor/spot/entrust.go
+++ plugin/dapp/zkspot/executor/spot/entrust.go
@@ -7,12 +7,13 @@ import (
 	et "github.com/33cn/plugin/plugin/dapp/zkspot/types"
 )
 
-var (
-	exchangeBindKeyPrefix = []byte("ebind-")
+const (
+	exchangeBindKeyPrefix = "ebind-"
 )
 
-func getBindKeyPrefix() []byte {
-	return exchangeBindKeyPrefix
+func (a *Entrust) getBindKeyPrefix() []byte {
+	key := a.prefix.GetStatedbPrefix() + exchangeBindKeyPrefix
+	return []byte(key)
 }
 
 type Entrust struct {
@@ -35,15 +36,6 @@ func (a *Entrust) SetDB(d dbm.KV, prefix et.DBprefix) {
 	a.prefix = prefix
 }
 
-// dummy
-func (a *Entrust) LimitOrder(payload *et.SpotLimitOrder, s string) (*types.Receipt, error) {
-	return nil, nil
-}
-
-func (a *Entrust) RevokeOrder(payload *et.SpotRevokeOrder) (*types.Receipt, error) {
-	return nil, nil
-}
-
 func (a *Entrust) Bind(payload *et.SpotExchangeBind) (*types.Receipt, error) {
 	if a.fromaddr != payload.GetExchangeAddress() {
 		return nil, types.ErrFromAddr
@@ -62,8 +54,8 @@ func (a *Entrust) Bind(payload *et.SpotExchangeBind) (*types.Receipt, error) {
 	log := getBindLog(payload, oldbind)
 	logs = append(logs, log)
 
-	saveBind(a.statedb, payload)
-	kv := getBindKV(payload)
+	a.saveBind(a.statedb, payload)
+	kv := a.getBindKV(payload)
 	kvs = append(kvs, kv...)
 
 	kvs = append(kvs, kv...)
@@ -86,7 +78,7 @@ func (a *Entrust) checkBind(entrustAddr, addr string) bool {
 }
 
 func (a *Entrust) getBind(addr string) string {
-	value, err := a.statedb.Get(bindKey(addr))
+	value, err := a.statedb.Get(a.bindKey(addr))
 	if err != nil || value == nil {
 		return ""
 	}
@@ -98,8 +90,8 @@ func (a *Entrust) getBind(addr string) string {
 	return bind.GetEntrustAddress()
 }
 
-func bindKey(id string) (key []byte) {
-	key = append(key, getBindKeyPrefix()...)
+func (a *Entrust) bindKey(id string) (key []byte) {
+	key = append(key, a.getBindKeyPrefix()...)
 	key = append(key, []byte(id)...)
 	return key
 }
@@ -115,15 +107,15 @@ func getBindLog(payload *et.SpotExchangeBind, old string) *types.ReceiptLog {
 	return log
 }
 
-func saveBind(db dbm.KV, payload *et.SpotExchangeBind) {
-	set := getBindKV(payload)
+func (a *Entrust) saveBind(db dbm.KV, payload *et.SpotExchangeBind) {
+	set := a.getBindKV(payload)
 	for i := 0; i < len(set); i++ {
 		db.Set(set[i].GetKey(), set[i].Value)
 	}
 }
 
-func getBindKV(payload *et.SpotExchangeBind) (kvset []*types.KeyValue) {
+func (a *Entrust) getBindKV(payload *et.SpotExchangeBind) (kvset []*types.KeyValue) {
 	value := types.Encode(payload)
-	kvset = append(kvset, &types.KeyValue{Key: bindKey(payload.ExchangeAddress), Value: value})
+	kvset = append(kvset, &types.KeyValue{Key: a.bindKey(payload.ExchangeAddress), Value: value})
 	return kvset
 }
