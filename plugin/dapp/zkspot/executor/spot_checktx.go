2ab7dfe89b628c79d91546acfa477b3ea53997aa
linj-disanbo <linj@disanbo.com>
2022-06-07 17:36:35 +0800

    fix import

--- plugin/dapp/zkspot/executor/spot_checktx.go
+++ plugin/dapp/zkspot/executor/spot_checktx.go
@@ -3,19 +3,18 @@ package executor
 import (
 	"github.com/33cn/chain33/types"
 	et "github.com/33cn/plugin/plugin/dapp/zkspot/types"
-	exchangetypes "github.com/33cn/plugin/plugin/dapp/zkspot/types"
 )
 
 // CheckTx 实现自定义检验交易接口，供框架调用
 func SpotCheckTx(cfg *types.Chain33Config, tx *types.Transaction, index int) error {
 	//发送交易的时候就检查payload,做严格的参数检查
-	var exchange exchangetypes.SpotAction
+	var exchange et.SpotAction
 	types.Decode(tx.GetPayload(), &exchange)
-	if exchange.Ty == exchangetypes.TyLimitOrderAction {
+	if exchange.Ty == et.TyLimitOrderAction {
 		limitOrder := exchange.GetLimitOrder()
 		return et.CheckLimitOrder(cfg, limitOrder)
 	}
-	if exchange.Ty == exchangetypes.TyMarketOrderAction {
+	if exchange.Ty == et.TyMarketOrderAction {
 		return types.ErrActionNotSupport
 	}
 	return nil
