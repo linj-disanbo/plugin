9e82c8bab3e47a8c23e67f927fdf231ba1c95b84
linj-disanbo <linj@disanbo.com>
2022-06-07 16:18:04 +0800

    add test

---
+++ plugin/dapp/zkspot/types/util_test.go
@@ -0,0 +1,17 @@
+package types
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSplitNFTContent(t *testing.T) {
+	hash := "7b8c47ff0f29187c4fd7b9404d6d8671c3a05d041a2126753722fe940f30e2d3"
+	fmt.Println("len", len(hash))
+	a, b, _, err := SplitNFTContent(hash)
+	assert.Nil(t, err)
+	t.Log("a", a.Text(16), "b", b.Text(16))
+	t.Log("a", a.BitLen(), "b", b.BitLen())
+}
