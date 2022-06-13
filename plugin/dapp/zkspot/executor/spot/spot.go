91780a1815e114ab3cb5bbd881a57b2a9d1e67dd
linj-disanbo <linj@disanbo.com>
2022-06-13 16:59:00 +0800

    update

--- plugin/dapp/zkspot/executor/spot/spot.go
+++ plugin/dapp/zkspot/executor/spot/spot.go
@@ -2,9 +2,7 @@ package spot
 
 import (
 	"encoding/hex"
-	"fmt"
 
-	"github.com/33cn/chain33/common/db/table"
 	log "github.com/33cn/chain33/common/log/log15"
 	drivers "github.com/33cn/chain33/system/dapp"
 	"github.com/33cn/chain33/types"
@@ -58,6 +56,25 @@ func (a *Spot) loadOrder(id int64) (*spotOrder, error) {
 
 	orderx := newSpotOrder(order, a.orderdb)
 	return orderx, nil
+}
+
+func (a *Spot) MatchLimitOrder(payload *et.SpotLimitOrder, taker *SpotTrader) (*types.Receipt, error) {
+	matcher1 := newMatcher(a.env.GetStateDB(), a.env.GetLocalDB(), a.env.GetAPI(), a.dbprefix)
+	elog.Info("LimitOrder", "height", a.env.GetHeight(), "order-price", payload.GetPrice(), "op", OpSwap(payload.Op), "index", taker.order.order.GetOrderID())
+	receipt1, err := matcher1.MatchLimitOrder(payload, taker, a.orderdb)
+	if err != nil {
+		return nil, err
+	}
+
+	if taker.order.isActiveOrder() {
+		receipt3, err := taker.FrozenForLimitOrder(taker.order)
+		if err != nil {
+			return nil, err
+		}
+		receipt1 = et.MergeReceipt(receipt1, receipt3)
+	}
+
+	return receipt1, nil
 }
 
 func (a *Spot) RevokeOrder(fromaddr string, payload *et.SpotRevokeOrder) (*types.Receipt, error) {
@@ -153,7 +170,7 @@ func (a *Spot) ExecLocal(tx *types.Transaction, receiptData *types.ReceiptData, 
 					elog.Error("updateIndex", "log.type.decode", err)
 					return nil, err
 				}
-				a.updateIndex(marketTable, orderTable, historyTable, receipt)
+				updateIndex(marketTable, orderTable, historyTable, receipt)
 			}
 		}
 	}
@@ -181,181 +198,6 @@ func (a *Spot) ExecLocal(tx *types.Transaction, receiptData *types.ReceiptData, 
 	kvs = append(kvs, kv...)
 	dbSet.KV = append(dbSet.KV, kvs...)
 	return dbSet, nil
-}
-
-func (a *Spot) updateIndex(marketTable, orderTable, historyTable *table.Table, receipt *et.ReceiptSpotMatch) (kvs []*types.KeyValue) {
-	elog.Info("updateIndex", "order.status", receipt.Order.Status)
-	switch receipt.Order.Status {
-	case et.Ordered:
-		err := a.updateOrder(marketTable, orderTable, historyTable, receipt.GetOrder(), receipt.GetIndex())
-		if err != nil {
-			return nil
-		}
-		err = a.updateMatchOrders(marketTable, orderTable, historyTable, receipt.GetOrder(), receipt.GetMatchOrders(), receipt.GetIndex())
-		if err != nil {
-			return nil
-		}
-	case et.Completed:
-		err := a.updateOrder(marketTable, orderTable, historyTable, receipt.GetOrder(), receipt.GetIndex())
-		if err != nil {
-			return nil
-		}
-		err = a.updateMatchOrders(marketTable, orderTable, historyTable, receipt.GetOrder(), receipt.GetMatchOrders(), receipt.GetIndex())
-		if err != nil {
-			return nil
-		}
-	case et.Revoked:
-		err := a.updateOrder(marketTable, orderTable, historyTable, receipt.GetOrder(), receipt.GetIndex())
-		if err != nil {
-			return nil
-		}
-	}
-
-	return
-}
-
-func (a *Spot) updateOrder(marketTable, orderTable, historyTable *table.Table, order *et.SpotOrder, index int64) error {
-	left := order.GetLimitOrder().GetLeftAsset()
-	right := order.GetLimitOrder().GetRightAsset()
-	op := order.GetLimitOrder().GetOp()
-	price := order.GetLimitOrder().GetPrice()
-	switch order.Status {
-	case et.Ordered:
-		var markDepth et.SpotMarketDepth
-		depth, err := getMarketDepth(marketTable, left, right, op, price)
-		if err == types.ErrNotFound {
-			markDepth.Price = price
-			markDepth.LeftAsset = left
-			markDepth.RightAsset = right
-			markDepth.Op = op
-			markDepth.Amount = order.Balance
-		} else {
-			markDepth.Price = price
-			markDepth.LeftAsset = left
-			markDepth.RightAsset = right
-			markDepth.Op = op
-			markDepth.Amount = depth.Amount + order.Balance
-		}
-		err = marketTable.Replace(&markDepth)
-		if err != nil {
-			elog.Error("updateIndex", "marketTable.Replace", err.Error())
-		}
-		err = orderTable.Replace(order)
-		if err != nil {
-			elog.Error("updateIndex", "orderTable.Replace", err.Error())
-		}
-
-	case et.Completed:
-		err := historyTable.Replace(order)
-		if err != nil {
-			elog.Error("updateIndex", "historyTable.Replace", err.Error())
-		}
-	case et.Revoked:
-		var marketDepth et.SpotMarketDepth
-		depth, err := getMarketDepth(marketTable, left, right, op, price)
-		if err == nil {
-			marketDepth.Price = price
-			marketDepth.LeftAsset = left
-			marketDepth.RightAsset = right
-			marketDepth.Op = op
-			marketDepth.Amount = depth.Amount - order.Balance
-
-			if marketDepth.Amount > 0 {
-				err = marketTable.Replace(&marketDepth)
-				if err != nil {
-					elog.Error("updateIndex", "marketTable.Replace", err.Error())
-				}
-			}
-			if marketDepth.Amount <= 0 {
-				err = marketTable.DelRow(&marketDepth)
-				if err != nil {
-					elog.Error("updateIndex", "marketTable.DelRow", err.Error())
-				}
-			}
-		}
-
-		primaryKey := []byte(fmt.Sprintf("%022d", order.OrderID))
-		err = orderTable.Del(primaryKey)
-		if err != nil {
-			elog.Error("updateIndex", "orderTable.Del", err.Error())
-		}
-		order.Status = et.Revoked
-		order.Index = index
-		err = historyTable.Replace(order)
-		if err != nil {
-			elog.Error("updateIndex", "historyTable.Replace", err.Error())
-		}
-	}
-	return nil
-}
-func (a *Spot) updateMatchOrders(marketTable, orderTable, historyTable *table.Table, order *et.SpotOrder, matchOrders []*et.SpotOrder, index int64) error {
-	left := order.GetLimitOrder().GetLeftAsset()
-	right := order.GetLimitOrder().GetRightAsset()
-	op := order.GetLimitOrder().GetOp()
-	if len(matchOrders) > 0 {
-		cache := make(map[int64]int64)
-		for i, matchOrder := range matchOrders {
-			if matchOrder.Balance == 0 && matchOrder.Executed == 0 {
-				var matchDepth et.SpotMarketDepth
-				matchDepth.Price = matchOrder.AVGPrice
-				matchDepth.LeftAsset = left
-				matchDepth.RightAsset = right
-				matchDepth.Op = OpSwap(op)
-				matchDepth.Amount = 0
-				err := marketTable.DelRow(&matchDepth)
-				if err != nil && err != types.ErrNotFound {
-					elog.Error("updateIndex", "marketTable.DelRow", err.Error())
-				}
-				continue
-			}
-			if matchOrder.Status == et.Completed {
-				err := orderTable.DelRow(matchOrder)
-				if err != nil {
-					elog.Error("updateIndex", "orderTable.DelRow", err.Error())
-				}
-				matchOrder.Index = index + int64(i+1)
-				err = historyTable.Replace(matchOrder)
-				if err != nil {
-					elog.Error("updateIndex", "historyTable.Replace", err.Error())
-				}
-			} else if matchOrder.Status == et.Ordered {
-				err := orderTable.Replace(matchOrder)
-				if err != nil {
-					elog.Error("updateIndex", "orderTable.Replace", err.Error())
-				}
-			}
-			executed := cache[matchOrder.GetLimitOrder().Price]
-			executed = executed + matchOrder.Executed
-			cache[matchOrder.GetLimitOrder().Price] = executed
-		}
-
-		for pr, executed := range cache {
-			var matchDepth et.SpotMarketDepth
-			depth, err := getMarketDepth(marketTable, left, right, OpSwap(op), pr)
-			if err != nil {
-				continue
-			} else {
-				matchDepth.Price = pr
-				matchDepth.LeftAsset = left
-				matchDepth.RightAsset = right
-				matchDepth.Op = OpSwap(op)
-				matchDepth.Amount = depth.Amount - executed
-			}
-			if matchDepth.Amount > 0 {
-				err = marketTable.Replace(&matchDepth)
-				if err != nil {
-					elog.Error("updateIndex", "marketTable.Replace", err.Error())
-				}
-			}
-			if matchDepth.Amount <= 0 {
-				err = marketTable.DelRow(&matchDepth)
-				if err != nil {
-					elog.Error("updateIndex", "marketTable.DelRow", err.Error())
-				}
-			}
-		}
-	}
-	return nil
 }
 
 func (a *Spot) LoadUser(fromaddr string, accountID uint64) (*SpotTrader, error) {
