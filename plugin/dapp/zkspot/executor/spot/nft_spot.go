3685a1586fd3adef2ba1e8adb9fd81a7564d7cc6
linj <linj@disanbo.com>
2022-07-07 20:36:18 +0800

    impl nft spot

---
+++ plugin/dapp/zkspot/executor/spot/nft_spot.go
@@ -0,0 +1,308 @@
+package spot
+
+import (
+	"encoding/hex"
+	"fmt"
+
+	"github.com/33cn/chain33/account"
+	dbm "github.com/33cn/chain33/common/db"
+	drivers "github.com/33cn/chain33/system/dapp"
+	"github.com/33cn/chain33/types"
+	et "github.com/33cn/plugin/plugin/dapp/zkspot/types"
+)
+
+type IGetSpotFee interface {
+	GetSpotFee(fromaddr string, left, right uint64) (*spotFee, error)
+}
+
+type NftSpot struct {
+	env      *drivers.DriverBase
+	tx       *et.TxInfo
+	dbprefix et.DBprefix
+	feeAcc   *SpotTrader
+	feeAcc2  *DexAccount
+
+	//
+	leftAccDb *EvmxgoNftAccountRepo
+	accountdb *accountRepo
+	orderdb   *orderSRepo
+	matcher1  *matcher
+	// fee
+	ExecAddr string
+}
+
+type EvmxgoNftAccountRepo struct {
+	cfg     *types.Chain33Config
+	statedb dbm.KV
+	symbol  string
+
+	accdb *account.DB
+}
+
+func newNftAccountRepo(db dbm.KV, cfg *types.Chain33Config) (*EvmxgoNftAccountRepo, error) {
+	return &EvmxgoNftAccountRepo{
+		statedb: db,
+		cfg:     cfg}, nil
+}
+
+func (accdb *EvmxgoNftAccountRepo) NewAccount(addr string, accid uint64, nftid uint64) (*NftAccount, error) {
+	var err error
+	symbol := fmt.Sprintf("%d", nftid)
+	if accdb.accdb == nil {
+		accdb.accdb, err = account.NewAccountDB(accdb.cfg, "evmxgo", symbol, accdb.statedb)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &NftAccount{accdb: accdb, address: addr, accid: accid, nftid: nftid, symbol: symbol}, nil
+}
+
+type NftAccount struct {
+	accdb   *EvmxgoNftAccountRepo
+	address string
+	accid   uint64
+	nftid   uint64
+	symbol  string
+}
+
+func NewNftSpot(e *drivers.DriverBase, tx *et.TxInfo, dbprefix et.DBprefix) (*NftSpot, error) {
+	leftAccDb, err := newNftAccountRepo(e.GetStateDB(), e.GetAPI().GetConfig())
+	if err != nil {
+		return nil, err
+	}
+	spot := &NftSpot{
+		env:       e,
+		tx:        tx,
+		dbprefix:  dbprefix,
+		leftAccDb: leftAccDb,
+		accountdb: newAccountRepo(spotDexName, e.GetStateDB(), dbprefix),
+		orderdb:   newOrderSRepo(e.GetStateDB(), dbprefix),
+		matcher1:  newMatcher(e.GetStateDB(), e.GetLocalDB(), e.GetAPI(), dbprefix),
+	}
+	return spot, nil
+}
+
+func (a *NftSpot) SetFeeAcc(funcGetFeeAccount GetFeeAccount) error {
+	feeAcc, err := funcGetFeeAccount()
+	if err != nil {
+		return err
+	}
+	a.feeAcc2 = feeAcc
+	return nil
+}
+
+func (a *NftSpot) loadOrder(id int64) (*spotOrder, error) {
+	order, err := a.orderdb.findOrderBy(id)
+	if err != nil {
+		return nil, err
+	}
+
+	orderx := newSpotOrder(order, a.orderdb)
+	return orderx, nil
+}
+
+func (a *NftSpot) GetSpotFee(fromaddr string, left, right uint64) (*spotFee, error) {
+	var takerFee, makerFee int32
+	takerFee, makerFee = 10000, 10000
+
+	return &spotFee{
+		addr:  a.feeAcc2.acc.Addr,
+		id:    a.feeAcc2.acc.Id,
+		taker: takerFee,
+		maker: makerFee,
+	}, nil
+}
+
+// execLocal ...
+func (a *NftSpot) ExecLocal(tx *types.Transaction, receiptData *types.ReceiptData, index int) (*types.LocalDBSet, error) {
+	dbSet := &types.LocalDBSet{}
+	historyTable := NewHistoryOrderTable(a.env.GetLocalDB(), a.dbprefix)
+	marketTable := NewMarketDepthTable(a.env.GetLocalDB(), a.dbprefix)
+	orderTable := NewMarketOrderTable(a.env.GetLocalDB(), a.dbprefix)
+	if receiptData.Ty == types.ExecOk {
+		for _, log := range receiptData.Logs {
+			switch log.Ty {
+			case et.TyMarketOrderLog, et.TyRevokeOrderLog, et.TyLimitOrderLog:
+				receipt := &et.ReceiptSpotMatch{}
+				if err := types.Decode(log.Log, receipt); err != nil {
+					elog.Error("updateIndex", "log.type.decode", err)
+					return nil, err
+				}
+				updateIndex(marketTable, orderTable, historyTable, receipt)
+			}
+		}
+	}
+
+	var kvs []*types.KeyValue
+	kv, err := marketTable.Save()
+	if err != nil {
+		elog.Error("updateIndex", "marketTable.Save", err.Error())
+		return nil, err
+	}
+	kvs = append(kvs, kv...)
+
+	kv, err = orderTable.Save()
+	if err != nil {
+		elog.Error("updateIndex", "orderTable.Save", err.Error())
+		return nil, err
+	}
+	kvs = append(kvs, kv...)
+
+	kv, err = historyTable.Save()
+	if err != nil {
+		elog.Error("updateIndex", "historyTable.Save", err.Error())
+		return nil, err
+	}
+	kvs = append(kvs, kv...)
+	dbSet.KV = append(dbSet.KV, kvs...)
+	return dbSet, nil
+}
+
+//GetIndex get index
+func (a *NftSpot) GetIndex() int64 {
+	// Add four zeros to match multiple MatchOrder indexes
+	return (a.env.GetHeight()*types.MaxTxsPerBlock + int64(a.tx.Index)) * 1e4
+}
+
+func (a *NftSpot) initOrder() func(*et.SpotOrder) *et.SpotOrder {
+	return func(order *et.SpotOrder) *et.SpotOrder {
+		order.OrderID = a.GetIndex()
+		order.Index = a.GetIndex()
+		order.CreateTime = a.env.GetBlockTime()
+		order.UpdateTime = a.env.GetBlockTime()
+		order.Hash = hex.EncodeToString(a.tx.Hash)
+		order.Addr = a.tx.From
+		return order
+	}
+}
+
+func (a *NftSpot) LoadNftTrader(fromaddr string, accountID uint64) (*NftSpotTraderHelper, error) {
+	acc, err := a.accountdb.LoadSpotAccount(fromaddr, accountID)
+	if err != nil {
+		elog.Error("executor/exchangedb LoadSpotAccount load taker account", "err", err)
+		return nil, err
+	}
+
+	return &NftSpotTraderHelper{
+		acc: acc,
+		cfg: a.env.GetAPI().GetConfig(),
+	}, nil
+}
+
+func (a *NftSpot) CreateNftOrder(fromaddr string, payload *et.SpotNftOrder, entrustAddr string) (*types.Receipt, error) {
+	fees, err := a.GetSpotFee(fromaddr, payload.LeftAsset, payload.RightAsset)
+	if err != nil {
+		elog.Error("executor/exchangedb getFees", "err", err)
+		return nil, err
+	}
+
+	order := createNftOrder(payload, et.TyNftOrderAction2, entrustAddr,
+		[]orderInit{a.initOrder(), fees.initLimitOrder()})
+
+	order2 := newSpotOrder(order, a.orderdb)
+	_, amount := order2.NeedToken(1)
+
+	trader, err := a.leftAccDb.NewAccount(fromaddr, payload.Order.AccountID, payload.LeftAsset)
+	if err != nil {
+		return nil, err
+	}
+
+	receipt3, err := trader.accdb.accdb.ExecFrozen(trader.address, a.ExecAddr, amount)
+	if err != nil {
+		return nil, err
+	}
+
+	matches := &et.ReceiptSpotMatch{
+		Order: order,
+		Index: a.GetIndex(),
+	}
+
+	receipt1, err := a.NftOrderReceipt(order2, matches)
+	if err != nil {
+		return nil, err
+	}
+	receipt1 = et.MergeReceipt(receipt1, receipt3)
+
+	return receipt1, nil
+}
+
+func (a *NftSpot) NftOrderReceipt(order *spotOrder, matches *et.ReceiptSpotMatch) (*types.Receipt, error) {
+	kvs := order.repo.GetOrderKvSet(order.order)
+	receiptlog := &types.ReceiptLog{Ty: et.TyNftOrderLog, Log: types.Encode(matches)}
+	receipts := &types.Receipt{Ty: types.ExecOk, KV: kvs, Logs: []*types.ReceiptLog{receiptlog}}
+	return receipts, nil
+}
+
+func (a *NftSpot) TradeNft(fromaddr string, taker *NftSpotTraderHelper, payload *et.SpotNftTakerOrder, entrustAddr string) (*types.Receipt, error) {
+	order2, err := a.orderdb.findOrderBy(payload.OrderID)
+	if err != nil {
+		elog.Error("TradeNft findOrderBy", "err", err, "orderid", payload.OrderID)
+		return nil, err
+	}
+
+	spotOrder2 := newSpotOrder(order2, a.orderdb)
+	if spotOrder2.isActiveOrder() {
+		return nil, et.ErrOrderID
+	}
+
+	order, err := a.CreateNftTakerOrder(fromaddr, taker, payload, entrustAddr)
+	if err != nil {
+		return nil, err
+	}
+	_ = order
+
+	log, kv, err := taker.Trade(a, spotOrder2)
+	return &types.Receipt{KV: kv, Logs: log}, nil
+}
+
+func (a *NftSpot) CreateNftTakerOrder(fromaddr string, acc *NftSpotTraderHelper, payload *et.SpotNftTakerOrder, entrustAddr string) (*et.SpotOrder, error) {
+	order2, err := a.orderdb.findOrderBy(payload.OrderID)
+	if err != nil {
+		elog.Error("CreateNftTakerOrder findOrderBy", "err", err, "orderid", payload.OrderID)
+		return nil, err
+	}
+
+	spotOrder2 := newSpotOrder(order2, a.orderdb)
+	if spotOrder2.isActiveOrder() {
+		return nil, et.ErrOrderID
+	}
+
+	fees, err := a.GetSpotFee(fromaddr, order2.GetNftOrder().LeftAsset, order2.GetNftOrder().RightAsset)
+	if err != nil {
+		elog.Error("CreateNftTakerOrder getFees", "err", err)
+		return nil, err
+	}
+	acc.fee = fees
+
+	order1 := createNftTakerOrder2(payload, entrustAddr, spotOrder2,
+		[]orderInit{a.initOrder(), fees.initLimitOrder()})
+	acc.order = newSpotOrder(order1, a.orderdb)
+
+	tid, amount := acc.order.nftTakerOrderNeedToken(spotOrder2, a.env.GetAPI().GetConfig().GetCoinPrecision())
+	err = acc.CheckTokenAmountForLimitOrder(tid, amount)
+	if err != nil {
+		return nil, err
+	}
+	acc.matches = &et.ReceiptSpotMatch{
+		Order: acc.order.order,
+		Index: a.GetIndex(),
+	}
+
+	return order1, nil
+}
+
+func createNftTakerOrder2(payload *et.SpotNftTakerOrder, entrustAddr string, order2 *spotOrder, inits []orderInit) *et.SpotOrder {
+	or := &et.SpotOrder{
+		Value:       &et.SpotOrder_NftTakerOrder{NftTakerOrder: payload},
+		Ty:          et.TyNftOrderAction2,
+		EntrustAddr: entrustAddr,
+		Executed:    0,
+		AVGPrice:    0,
+		Balance:     order2.order.Balance,
+		Status:      et.Ordered,
+	}
+	for _, initFun := range inits {
+		or = initFun(or)
+	}
+	return or
+}
---
+++ plugin/dapp/zkspot/executor/spot/nft_spot_trade.go
@@ -0,0 +1,170 @@
+package spot
+
+import (
+	"github.com/33cn/chain33/types"
+	et "github.com/33cn/plugin/plugin/dapp/zkspot/types"
+)
+
+// 同步处理 order account 变化， 并收取fee
+type NftSpotTraderHelper struct {
+	cfg   *types.Chain33Config
+	acc   *DexAccount
+	order *spotOrder
+	fee   *spotFee
+
+	matches *et.ReceiptSpotMatch
+	accFee  *DexAccount
+}
+
+func (s *NftSpotTraderHelper) Trade(spot *NftSpot, makerOrder *spotOrder) ([]*types.ReceiptLog, []*types.KeyValue, error) {
+	balance := s.calcTradeBalance(makerOrder.order)
+	matchDetail := s.calcTradeInfo(makerOrder, balance)
+
+	receipt3, kvs3, err := makerOrder.Traded(matchDetail, s.order.order.UpdateTime)
+	if err != nil {
+		elog.Error("maker.orderTraded", "err", err)
+		return receipt3, kvs3, err
+	}
+
+	receipt2, kvs2, err := s.takerOrderTraded(matchDetail, makerOrder.order)
+	if err != nil {
+		elog.Error("taker.orderTraded", "err", err)
+		return receipt2, kvs2, err
+	}
+
+	makerNftAcc, err := spot.leftAccDb.NewAccount(makerOrder.order.Addr, uint64(makerOrder.order.GetNftOrder().Order.AccountID), makerOrder.order.GetNftOrder().LeftAsset)
+	if err != nil {
+		elog.Error("load maker nft account", "err", err)
+		return receipt2, kvs2, err
+	}
+	takerNftAcc, err := spot.leftAccDb.NewAccount(s.order.order.Addr, uint64(s.order.order.GetNftTakerOrder().Order.AccountID), makerOrder.order.GetNftOrder().LeftAsset)
+	if err != nil {
+		elog.Error("load taker nft account", "err", err)
+		return receipt2, kvs2, err
+	}
+
+	makerAcc, err := spot.accountdb.LoadAccount(s.order.order.Addr, uint64(s.order.order.GetNftTakerOrder().Order.AccountID))
+	if err != nil {
+		elog.Error("load makerAcc  account", "err", err)
+		return receipt2, kvs2, err
+	}
+
+	receipt, kvs, err := s.settlement(takerNftAcc, makerNftAcc, makerAcc, matchDetail, makerOrder)
+	if err != nil {
+		elog.Error("settlement", "err", err)
+		return receipt, kvs, err
+	}
+
+	kvs = append(kvs, kvs2...)
+	kvs = append(kvs, kvs3...)
+	receipt = append(receipt, receipt2...)
+	receipt = append(receipt, receipt3...)
+
+	return receipt, kvs, nil
+}
+
+func (s *NftSpotTraderHelper) calcTradeBalance(order *et.SpotOrder) int64 {
+	return order.GetBalance()
+}
+
+func (s *NftSpotTraderHelper) calcTradeInfo(makerOrder *spotOrder, balance int64) *et.MatchInfo {
+	price := makerOrder.order.GetNftOrder().Price
+	var info et.MatchInfo
+	info.Matched = balance
+	info.LeftBalance = balance
+	info.Price = price
+	info.RightBalance = SafeMul(info.Price, balance, 1)
+	info.FeeTaker = SafeMul(info.RightBalance, int64(s.order.order.TakerRate), s.cfg.GetCoinPrecision())
+	info.FeeMaker = SafeMul(info.RightBalance, int64(makerOrder.order.Rate), s.cfg.GetCoinPrecision())
+	return &info
+}
+
+// settlement
+// LeftToken: seller -> buyer
+// RightToken: buyer -> seller
+// RightToken: buyer, seller -> fee-bank
+func (s *NftSpotTraderHelper) settlement(takerNft, makerNft *NftAccount, makerAcc *DexAccount, tradeBalance *et.MatchInfo, makerOrder *spotOrder) ([]*types.ReceiptLog, []*types.KeyValue, error) {
+	// not support taker/maker the same user
+	if s.acc.acc.Id == makerNft.accid {
+		return nil, nil, types.ErrNotSupport
+	}
+
+	copyAcc := dupAccount(s.acc.acc)
+	copyAccMaker := dupAccount(makerAcc.acc)
+	copyFeeAcc := dupAccount(s.accFee.acc)
+
+	_, rightToken := makerOrder.order.GetNftOrder().LeftAsset, makerOrder.order.GetNftOrder().RightAsset
+	var err error
+	// op always := et.OpBuy
+	err = s.acc.doTranfer(makerAcc, rightToken, uint64(tradeBalance.RightBalance))
+	if err != nil {
+		elog.Error("settlement", "buy.doTranfer1", err)
+		return nil, nil, err
+	}
+	receiptN, err := makerNft.accdb.accdb.ExecTransferFrozen(makerNft.address, takerNft.address, "mxxx", tradeBalance.LeftBalance)
+	if err != nil {
+		elog.Error("settlement", "buy.doFrozenTranfer2", err)
+		return nil, nil, err
+	}
+	err = s.acc.doTranfer(s.accFee, rightToken, uint64(tradeBalance.FeeTaker))
+	if err != nil {
+		elog.Error("settlement", "buy-fee.doTranfer1", err)
+		return nil, nil, err
+	}
+	err = makerAcc.doTranfer(s.accFee, rightToken, uint64(tradeBalance.FeeMaker))
+	if err != nil {
+		elog.Error("settlement", "buy-fee.doTranfer2", err)
+		return nil, nil, err
+	}
+
+	kvs1 := s.acc.GetKVSet()
+	kvs2 := makerAcc.GetKVSet()
+	kvs3 := s.accFee.GetKVSet()
+
+	kvs1 = append(kvs1, kvs2...)
+	kvs1 = append(kvs1, kvs3...)
+	kvs1 = append(kvs1, receiptN.KV...)
+
+	re := et.ReceiptSpotTrade{
+		Match: tradeBalance,
+		Prev: &et.TradeAccounts{
+			Taker: copyAcc,
+			Maker: copyAccMaker,
+			Fee:   copyFeeAcc,
+		},
+		Current: &et.TradeAccounts{
+			Taker: s.acc.acc,
+			Maker: makerAcc.acc,
+			Fee:   s.accFee.acc,
+		},
+		MakerOrder: makerOrder.order.GetNftOrder().Order,
+	}
+
+	log1 := types.ReceiptLog{
+		Ty:  et.TySpotTradeLog,
+		Log: types.Encode(&re),
+	}
+	var logs []*types.ReceiptLog
+	logs = append(logs, &log1)
+	logs = append(logs, receiptN.Logs...)
+
+	return []*types.ReceiptLog{&log1}, kvs1, nil
+}
+
+func (s *NftSpotTraderHelper) takerOrderTraded(matchDetail *et.MatchInfo, order *et.SpotOrder) ([]*types.ReceiptLog, []*types.KeyValue, error) {
+	s.order.orderUpdate(matchDetail)
+	s.matches.Order = s.order.order
+	s.matches.MatchOrders = append(s.matches.MatchOrders, order)
+	// receipt-log, order-kvs 在匹配完成后一次性生成, 不需要生成多次
+	// kvs := GetOrderKvSet(s.order)
+	// logs += s.matches
+	return []*types.ReceiptLog{}, []*types.KeyValue{}, nil
+}
+
+func (s *NftSpotTraderHelper) CheckTokenAmountForLimitOrder(tid uint64, total int64) error {
+	if s.acc.getBalance(tid) < uint64(total) {
+		elog.Error("limit check right balance", "addr", s.acc.acc.Addr, "avail", s.acc.acc.Balance, "b", s.acc.getBalance(tid), "need", total)
+		return et.ErrAssetBalance
+	}
+	return nil
+}
