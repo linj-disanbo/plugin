9f2746c4c7b06a49d64a67473251391627672fff
linj <linj@disanbo.com>
2022-07-11 16:47:31 +0800

    fix get nft order

--- plugin/dapp/zkspot/executor/spot/nft_spot.go
+++ plugin/dapp/zkspot/executor/spot/nft_spot.go
@@ -238,15 +238,15 @@ func (a *NftSpot) NftOrderReceipt(order *spotOrder, matches *et.ReceiptSpotMatch
 }
 
 func (a *NftSpot) TradeNft(fromaddr string, taker *NftSpotTraderHelper, payload *et.SpotNftTakerOrder, entrustAddr string) (*types.Receipt, error) {
-	order2, err := a.orderdb.findOrderBy(payload.OrderID)
+	order2, err := a.orderdb.findNftOrderBy(payload.OrderID)
 	if err != nil {
-		elog.Error("TradeNft findOrderBy", "err", err, "orderid", payload.OrderID)
+		elog.Error("TradeNft findNftOrderBy", "err", err, "orderid", payload.OrderID)
 		return nil, err
 	}
 
 	spotOrder2 := newSpotOrder(order2, a.orderdb)
 	if spotOrder2.isActiveOrder() {
-		elog.Error("TradeNft findOrderBy", "err", et.ErrOrderID, "orderid", payload.OrderID)
+		elog.Error("TradeNft findNftOrderBy", "err", et.ErrOrderID, "orderid", payload.OrderID)
 		return nil, et.ErrOrderID
 	}
 
